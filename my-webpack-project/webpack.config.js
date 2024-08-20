5df012eb793949f9825ff11026214de9b96c86ad
zoeznm <bomikim046@gmail.com>
2024-08-20 21:58:48 +0900

    aws를 위한 번들링

---
+++ my-webpack-project/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'development',  // 또는 'development'로 변경할 수 있습니다.
+};
